47e2930114811d27a1c4e1a337e310f74f282048
Jerven bolleman <jerven.bolleman@sib.swiss>
2020-03-04 20:57:51 +0100

    Working on #955 Parsing passes, now into the algebra

--- test/test_sparql_star.py
+++ test/test_sparql_star.py
@@ -1,26 +1,40 @@
 from rdflib import Graph
+from rdflib.plugins.sparql.parser import VarOrBlankNodeOrIriOrLitOrEmbTP
+import  pyparsing
+import unittest
 
+class SparqlStarTests(unittest.TestCase):
 
-def test_basic_sparql_star_subject():
-    g = Graph()
-    res = g.query('''SELECT * WHERE {
-<< ?s ?p ?o >> ?p2 ?o2 . 
-}''')
-
-
-def test_basic_sparql_star_object():
-    g = Graph()
-    res = g.query('''SELECT * WHERE {
-?s ?p << ?s2 ?p2 ?o2 >> . 
-}''')
-
-def test_basic_sparql():
-    g = Graph()
-    res = g.query('''SELECT * WHERE {
-    ?s ?p ?o . 
+    @staticmethod
+    def test_basic_sparql_star_subject():
+        g = Graph()
+        res = g.query('''SELECT * WHERE {
+    << ?s ?p ?o >> ?p2 ?o2 . 
     }''')
 
+    @staticmethod
+    def test_basic_sparql_star_object():
+        g = Graph()
+        res = g.query('''SELECT * WHERE {
+    ?s ?p << ?s2 ?p2 ?o2 >> . 
+    }''')
 
-def test_bind_sparql_star():
-    g = Graph()
-    res = g.query('''SELECT * WHERE { BIND(<< ?s2 ?p2 ?o2 >> AS ?b) }''')
+    @staticmethod
+    def test_basic_sparql():
+        g = Graph()
+        res = g.query('''SELECT * WHERE {
+        ?s ?p ?o . 
+        }''')
+
+    @staticmethod
+    def test_bind_sparql_star():
+        g = Graph()
+        res = g.query('''SELECT * WHERE { BIND(<< ?s2 ?p2 ?o2 >> AS ?b) }''')
+
+    @staticmethod
+    def test_parser():
+        assert VarOrBlankNodeOrIriOrLitOrEmbTP.matches("<<?s ?p ?o>>")
+
+
+if __name__ == '__main__':
+    unittest.main()
